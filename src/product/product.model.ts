8f6b7ba51a18c31a65b8b50bb809baf831ee0b47
roman-cash <rkulikov2013@gmail.com>
2022-10-27 11:30:05 +0300

    add: models

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,1 +1,16 @@
-export class ProductModel {}
+export class ProductModel {
+	image: string;
+	title: string;
+	price: number;
+	oldPrice: number;
+	credits: number;
+	calculateRating: number;
+	description: string;
+	advantages: string;
+	disAdvantages: string;
+	categories: string[];
+	tags: string[];
+	characteristics: {
+		[key: string]: string
+	};
+}
