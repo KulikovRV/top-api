13d419707cd80fce05b25304b1a1b5eb6e6e712a
roman-cash <rkulikov2013@gmail.com>
2022-11-15 16:38:33 +0300

    add: top-page service

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,32 +1,75 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
-import { TopPageModule } from './top-page.module';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	NotFoundException,
+	Param,
+	Patch,
+	Post, UseGuards,
+	UsePipes, ValidationPipe,
+} from '@nestjs/common';
 import { FindTopPageDto } from './dto/find-top-page.dto';
+import { IdValidationPipe } from '../pipes/id-validation.pipe';
+import { CreateTopPageDto } from './dto/create-top-page.dto';
+import { TopPageService } from './top-page.service';
+import { TOP_PAGE_NOT_FOUND_ERROR } from './top-page.constants';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
 @Controller('top-page')
 export class TopPageController {
+	constructor(private readonly topPageService: TopPageService) {}
+
+	@UseGuards(JwtAuthGuard)
 	@Post('create')
-	async create(@Body() dto: Omit<TopPageModule, '_id'>) {
-
+	async create(@Body() dto: CreateTopPageDto) {
+		return this.topPageService.create(dto);
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Get(':id')
-	async get(@Param('id') id: string) {
-
+	async get(@Param('id', IdValidationPipe) id: string) {
+		const page = await this.topPageService.findById(id);
+		if (!page) {
+			throw new NotFoundException(TOP_PAGE_NOT_FOUND_ERROR);
+		}
+		return page;
 	}
 
+	@Get('byAlias/:alias')
+	async getByAlias(@Param('alias') alias: string) {
+		const page = await this.topPageService.findByAlias(alias);
+		if (!page) {
+			throw new NotFoundException(TOP_PAGE_NOT_FOUND_ERROR);
+		}
+		return page;
+	}
+
+	@UseGuards(JwtAuthGuard)
 	@Delete(':id')
-	async delete(@Param('id') id: string) {
-
+	async delete(@Param('id', IdValidationPipe) id: string) {
+		const page = await this.topPageService.deleteById(id);
+		if (!page) {
+			throw new NotFoundException(TOP_PAGE_NOT_FOUND_ERROR);
+		}
+		return page;
 	}
 
+	@UseGuards(JwtAuthGuard)
 	@Patch(':id')
-	async patch(@Param('id') id: string, @Body() dto: TopPageModule) {
-
+	async patch(@Param('id') id: string, @Body() dto: CreateTopPageDto) {
+		const updatedPage = await this.topPageService.updateById(id, dto);
+		if (!updatedPage) {
+			throw new NotFoundException(TOP_PAGE_NOT_FOUND_ERROR);
+		}
+		return updatedPage;
 	}
 
+	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
-	@Post()
+	@Post('find')
 	async find(@Body() dto: FindTopPageDto) {
-
+		return this.topPageService.findByCategory(dto.firstCategory);
 	}
 }
